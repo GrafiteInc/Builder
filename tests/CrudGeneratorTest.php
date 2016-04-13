cb40b79b1e519ee802c59c883e4f2d47e4930609
mlantz <mattlantz@gmail.com>
2016-04-13 16:53:51 -0400

    Missing part of test

--- tests/CrudGeneratorTest.php
+++ tests/CrudGeneratorTest.php
@@ -11,6 +11,7 @@ class CrudGeneratorTest extends PHPUnit_Framework_TestCase
     {
         $this->generator = new CrudGenerator();
         $this->config = [
+            'bootstrap'                  => false,
             '_path_facade_'              => vfsStream::url('Facades'),
             '_path_service_'             => vfsStream::url('Services'),
             '_path_repository_'          => vfsStream::url('Repositories/'.ucfirst('testTable')),
