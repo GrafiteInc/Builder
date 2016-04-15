24ea81316a97812280a368477a5c833f6b99824e
mlantz <mattlantz@gmail.com>
2016-04-14 23:31:20 -0400

    Bootstrap improvements

--- src/Bootstrap/resources/views/dashboard/panel.blade.php
+++ src/Bootstrap/resources/views/dashboard/panel.blade.php
@@ -1,7 +1,7 @@
 <ul class="nav nav-sidebar">
     <li><a href="{!! url('dashboard') !!}"><span class="fa fa-dashboard"></span> Dashboard</a></li>
     <li><a href="{!! url('user/settings') !!}"><span class="fa fa-user"></span> Settings</a></li>
+    <li><a href="{!! url('teams') !!}"><span class="fa fa-users"></span> Teams</a></li>
     <li class="sidebar-header"><span>Admin</span></li>
-    <li><a href="{!! url('admin/users') !!}"><span class="fa fa-users"></span> Admin</a></li>
-    <li><a href="{!! url('teams') !!}"><span class="fa fa-users"></span> Team Manager</a></li>
+    <li><a href="{!! url('admin/users') !!}"><span class="fa fa-users"></span> Users</a></li>
 </ul>
