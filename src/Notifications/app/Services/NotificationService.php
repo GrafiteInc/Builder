8fb8766f01a11812d3e3ce57ea0856515302c8c6
mlantz <mattlantz@gmail.com>
2016-06-01 17:53:54 -0400

    Doc blocks

--- src/Notifications/app/Services/NotificationService.php
+++ src/Notifications/app/Services/NotificationService.php
@@ -16,31 +16,69 @@ class NotificationService
         $this->userService = $userService;
     }
 
+    /**
+     * All notifications
+     *
+     * @return  Collection
+     */
     public function all()
     {
         return $this->repo->all();
     }
 
+    /**
+     * Paginated notifications
+     *
+     * @return  PaginatedCollection
+     */
     public function paginated()
     {
         return $this->repo->paginated(env('paginate', 25));
     }
 
+    /**
+     * User based paginated notifications
+     *
+     * @param  integer $id
+     * @return  PaginatedCollection
+     */
     public function userBasedPaginated($id)
     {
         return $this->repo->userBasedPaginated($id, env('paginate', 25));
     }
 
+    /**
+     * User based notifications
+     *
+     * @param  integer $id
+     * @return  Collection
+     */
     public function userBased($id)
     {
         return $this->repo->userBased($id);
     }
 
+    /**
+     * Search notifications
+     *
+     * @param  string $input
+     * @param  integer $id
+     * @return  Collection
+     */
     public function search($input, $id)
     {
         return $this->repo->search($input, env('paginate', 25), $id);
     }
 
+    /**
+     * Create a notificaton
+     *
+     * @param  integer $userId
+     * @param  string $flag
+     * @param  string $title
+     * @param  string $details
+     * @return  void
+     */
     public function notify($userId, $flag, $title, $details)
     {
         $input = [
@@ -52,6 +90,12 @@ class NotificationService
         $this->create($input);
     }
 
+    /**
+     * Create a notification
+     *
+     * @param  array $input
+     * @return  boolean|exception
+     */
     public function create($input)
     {
         try {
@@ -74,37 +118,79 @@ class NotificationService
         }
     }
 
+    /**
+     * Get a user
+     *
+     * @param  integer $id
+     * @return  User
+     */
     public function getUser($id)
     {
         return $this->userService->find($id);
     }
 
+    /**
+     * Find a notification
+     *
+     * @param  integer $id
+     * @return  Notification
+     */
     public function find($id)
     {
         return $this->repo->find($id);
     }
 
+    /**
+     * Find a notification by UUID
+     *
+     * @param  string $uuid
+     * @return  Notification
+     */
     public function findByUuid($uuid)
     {
         return $this->repo->findByUuid($uuid);
     }
 
+    /**
+     * Update a notification
+     *
+     * @param  integer $id
+     * @param  array $input
+     * @return  Notification
+     */
     public function update($id, $input)
     {
         return $this->repo->update($id, $input);
     }
 
+    /**
+     * Mark notification as read
+     *
+     * @param  integer $id
+     * @return  boolean
+     */
     public function markAsRead($id)
     {
         $input['is_read'] = true;
         return $this->repo->update($id, $input);
     }
 
+    /**
+     * Destroy a Notification
+     *
+     * @param  integer $id
+     * @return  boolean
+     */
     public function destroy($id)
     {
         return $this->repo->destroy($id);
     }
 
+    /**
+     * Users as Select options array
+     *
+     * @return  Array
+     */
     public function usersAsOptions()
     {
         $users = ['All' => 0];
