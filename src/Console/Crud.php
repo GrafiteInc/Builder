b42a370b55c9484d61ab949e2c603f1d9c3be49d
Casper Boone <mail@casperboone.nl>
2016-06-24 20:55:11 +0000

    Fix default service only tests

--- src/Console/Crud.php
+++ src/Console/Crud.php
@@ -137,7 +137,7 @@ class Crud extends Command
             '_camel_case_'               => ucfirst(camel_case($table)),
             '_camel_casePlural_'         => str_plural(camel_case($table)),
             '_ucCamel_casePlural_'       => ucfirst(str_plural(camel_case($table))),
-            'service_only_tests'         => 'Integration/RepositoryIntegrationTest,Integration/ServiceIntegrationTest'g
+            'service_only_tests'         => 'Integration/RepositoryIntegrationTest,Integration/ServiceIntegrationTest'
         ];
 
         $templateDirectory = __DIR__.'/../Templates';
