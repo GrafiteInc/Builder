e4b5ff2e1cc7d2e553ad5a11d53f9a8b5278e674
Matt Lantz <mattlantz@gmail.com>
2016-07-14 16:59:09 -0400

    Weird changes

--- tests/CrudGeneratorTest.php
+++ tests/CrudGeneratorTest.php
@@ -26,12 +26,12 @@ class CrudGeneratorTest extends PHPUnit_Framework_TestCase
             '_path_repository_'          => vfsStream::url('Repositories/'.ucfirst('testTable')),
             '_path_model_'               => vfsStream::url('Repositories/'.ucfirst('testTable')),
             '_path_controller_'          => vfsStream::url('Http/Controllers'),
-            '_path_api_controller_'      => vfsStream::url('Http/Controllers/Api'),
+            '_path_api_controller_'      => vfsStream::url('routes/Http/Controllers/Api'),
             '_path_views_'               => vfsStream::url('resources/views'),
             '_path_tests_'               => vfsStream::url('tests'),
             '_path_request_'             => vfsStream::url('Http/Requests'),
-            '_path_routes_'              => vfsStream::url('Http/routes.php'),
-            '_path_api_routes_'          => vfsStream::url('Http/api-routes.php'),
+            '_path_routes_'              => vfsStream::url('routes/web.php'),
+            '_path_api_routes_'          => vfsStream::url('routes/api.php'),
             'routes_prefix'              => '',
             'routes_suffix'              => '',
             '_namespace_services_'       => 'App\Services',
@@ -51,7 +51,9 @@ class CrudGeneratorTest extends PHPUnit_Framework_TestCase
 
     public function testApiGenerator()
     {
-        $this->crud = vfsStream::setup("Http/Controllers/Api");
+        $this->crud = vfsStream::setup('routes');
+        vfsStream::newFile('api.php')->at($this->crud)->setContent("<?php\n\n\n");
+        vfsStream::newFile('Http/Controllers/Api/TestTableController.php')->at($this->crud)->setContent("<?php\n\n\n");
         $this->generator->createApi($this->config, false);
         $this->assertTrue($this->crud->hasChild('Http/Controllers/Api/TestTableController.php'));
         $contents = $this->crud->getChild('Http/Controllers/Api/TestTableController.php');
@@ -96,10 +98,10 @@ class CrudGeneratorTest extends PHPUnit_Framework_TestCase
 
     public function testRoutesGenerator()
     {
-        $this->crud = vfsStream::setup("Http");
-        file_put_contents(vfsStream::url('Http/routes.php'), 'test');
+        $this->crud = vfsStream::setup("routes");
+        file_put_contents(vfsStream::url('routes/web.php'), 'test');
         $this->generator->createRoutes($this->config, false);
-        $contents = $this->crud->getChild('Http/routes.php');
+        $contents = $this->crud->getChild('routes/web.php');
         $this->assertTrue(strpos($contents->getContent(), 'TestTableController') !== false);
 
         // Ensure Search Route specification exists and Controller and Action remain
