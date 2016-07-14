551c9419727da0bb5b6b48f9a0b7152feacf2061
Matt Lantz <mattlantz@gmail.com>
2016-07-14 17:17:54 -0400

    Moved auth around

--- src/Packages/Starter/app/Http/Controllers/Auth/RegisterController.php
+++ src/Packages/Starter/app/Http/Controllers/Auth/RegisterController.php
@@ -7,36 +7,38 @@ use Validator;
 use {{App\}}Services\UserService;
 use {{App\}}Repositories\User\User;
 use {{App\}}Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\ThrottlesLogins;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Foundation\Auth\RegistersUsers;
 
-class AuthController extends Controller
+class RegisterController extends Controller
 {
     /*
     |--------------------------------------------------------------------------
-    | Registration & Login Controller
+    | Register Controller
     |--------------------------------------------------------------------------
     |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
     |
     */
 
-    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
-
-    protected $loginPath = '/login';
-
-    protected $redirectPath = '/dashboard';
+    use RegistersUsers;
 
     /**
-     * Create a new authentication controller instance.
+     * Where to redirect users after login / registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
      *
      * @return void
      */
     public function __construct(UserService $userService)
     {
-        $this->middleware('guest', ['except' => 'getLogout']);
+        $this->middleware('guest');
         $this->service = $userService;
     }
 
@@ -51,7 +53,7 @@ class AuthController extends Controller
         return Validator::make($data, [
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|confirmed|min:6',
+            'password' => 'required|min:6|confirmed',
         ]);
     }
 
