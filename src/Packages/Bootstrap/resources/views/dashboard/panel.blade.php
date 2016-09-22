33c451917b63569eedd7edf77cc8bca12fbdb6d1
Matt Lantz <mattlantz@gmail.com>
2016-09-22 11:03:01 -0400

    dashboard panel improvement

--- src/Packages/Bootstrap/resources/views/dashboard/panel.blade.php
+++ src/Packages/Bootstrap/resources/views/dashboard/panel.blade.php
@@ -2,7 +2,9 @@
     <li><a href="{!! url('dashboard') !!}"><span class="fa fa-dashboard"></span> Dashboard</a></li>
     <li><a href="{!! url('user/settings') !!}"><span class="fa fa-user"></span> Settings</a></li>
     <li><a href="{!! url('teams') !!}"><span class="fa fa-users"></span> Teams</a></li>
-    <li class="sidebar-header"><span>Admin</span></li>
-    <li><a href="{!! url('admin/users') !!}"><span class="fa fa-users"></span> Users</a></li>
-    <li><a href="{!! url('admin/roles') !!}"><span class="fa fa-lock"></span> Roles</a></li>
+    @if (Gate::allows('admin'))
+        <li class="sidebar-header"><span>Admin</span></li>
+        <li><a href="{!! url('admin/users') !!}"><span class="fa fa-users"></span> Users</a></li>
+        <li><a href="{!! url('admin/roles') !!}"><span class="fa fa-lock"></span> Roles</a></li>
+    @endif
 </ul>
