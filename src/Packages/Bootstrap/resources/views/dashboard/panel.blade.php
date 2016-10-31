987d37bf9604b483bc7a9949882af809e77ae832
Matt Lantz <mattlantz@gmail.com>
2016-10-31 07:23:45 -0400

    Minor bootstrap improvements

--- src/Packages/Bootstrap/resources/views/dashboard/panel.blade.php
+++ src/Packages/Bootstrap/resources/views/dashboard/panel.blade.php
@@ -1,10 +1,20 @@
 <ul class="nav nav-sidebar">
-    <li><a href="{!! url('dashboard') !!}"><span class="fa fa-dashboard"></span> Dashboard</a></li>
-    <li><a href="{!! url('user/settings') !!}"><span class="fa fa-user"></span> Settings</a></li>
-    <li><a href="{!! url('teams') !!}"><span class="fa fa-users"></span> Teams</a></li>
+    <li @if(Request::is('dashboard', 'dashboard/*')) class="active" @endif>
+        <a href="{!! url('dashboard') !!}"><span class="fa fa-dashboard"></span> Dashboard</a>
+    </li>
+    <li @if(Request::is('user/settings', 'user/password')) class="active" @endif>
+        <a href="{!! url('user/settings') !!}"><span class="fa fa-user"></span> Settings</a>
+    </li>
+    <li @if(Request::is('teams', 'teams/*')) class="active" @endif>
+        <a href="{!! url('teams') !!}"><span class="fa fa-users"></span> Teams</a>
+    </li>
     @if (Gate::allows('admin'))
         <li class="sidebar-header"><span>Admin</span></li>
-        <li><a href="{!! url('admin/users') !!}"><span class="fa fa-users"></span> Users</a></li>
-        <li><a href="{!! url('admin/roles') !!}"><span class="fa fa-lock"></span> Roles</a></li>
+        <li @if(Request::is('admin/users', 'admin/users/*')) class="active" @endif>
+            <a href="{!! url('admin/users') !!}"><span class="fa fa-users"></span> Users</a>
+        </li>
+        <li @if(Request::is('admin/roles', 'admin/roles/*')) class="active" @endif>
+            <a href="{!! url('admin/roles') !!}"><span class="fa fa-lock"></span> Roles</a>
+        </li>
     @endif
 </ul>
