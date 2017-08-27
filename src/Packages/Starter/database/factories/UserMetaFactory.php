0069b31ef70f3b56be221f05d26b7f07876e4d5c
Matt Lantz <mattlantz@gmail.com>
2017-08-27 00:49:18 -0400

    Preparation for Laravel 5.5

---
+++ src/Packages/Starter/database/factories/UserMetaFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+use Faker\Generator as Faker;
+
+
+/*
+|--------------------------------------------------------------------------
+| UserMeta Factory
+|--------------------------------------------------------------------------
+*/
+
+$factory->define({{App\}}Models\UserMeta::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => 1,
+        'phone' => $faker->phoneNumber,
+        'marketing' => 1,
+        'terms_and_cond' => 1,
+    ];
+});
