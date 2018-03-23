2093f887cd23791cf51418e704407921c932d8a1
Matt Lantz <mattlantz@gmail.com>
2018-03-22 22:17:31 -0400

    Bootstrap clean up

--- src/Packages/Bootstrap/resources/views/layouts/navigation.blade.php
+++ src/Packages/Bootstrap/resources/views/layouts/navigation.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="/"><span class="fa fa-cogs"></span> Brand</a>
+    <a class="navbar-brand mr-0" href="/"><span class="fa fa-cogs"></span> Brand</a>
     <ul class="navbar-nav mr-auto">
         <span class="navbar-text raw-margin-left-24 page-title">
             <a class="sidebar-toggle text-light mr-3"><i class="fa fa-bars"></i></a>
