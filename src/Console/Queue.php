9f38ad4396dd19cb03c526b73743d01a60b79db3
Matt Lantz <mattlantz@gmail.com>
2018-05-13 19:55:13 -0400

    Started the new queue manager

---
+++ src/Console/Queue.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Grafite\Builder\Console;
+
+use Grafite\Builder\Console\GrafiteCommand;
+use Grafite\Builder\Traits\FileMakerTrait;
+use Illuminate\Filesystem\Filesystem;
+
+class Queue extends GrafiteCommand
+{
+    use FileMakerTrait;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'grafite:queue';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Grafite Builder will add a database driven queue manager for your app';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        if ($this->starterIsInstalled()) {
+            $fileSystem = new Filesystem();
+
+            $files = $fileSystem->allFiles(__DIR__.'/../Packages/Queue');
+            $this->line("\n");
+            foreach ($files as $file) {
+                $this->line(str_replace(__DIR__.'/../Packages/Queue/', '', $file));
+            }
+
+            $this->info("\n\nThese files will be published\n");
+
+            $result = $this->confirm('Are you sure you want to overwrite any files of the same name?');
+
+            if ($result) {
+                $this->copyPreparedFiles(__DIR__.'/../Packages/Queue', base_path());
+                $this->info("\n\n Please review the setup details for queue.");
+                $this->info("\n\n You will want to add things like:");
+                $this->line("\n These links: ");
+                $this->comment("\n <li><a class=\"nav-link\" href='{!! url('admin/queue') !!}'><span class='fa fa-chart'></span> Queue</a></li>");
+                $this->line("\n Now modify the RouteServiceProvider by switching to a closure in the `group` method (app/Providers/RouteServiceProvider.php):");
+                $this->line("\n It will look like: ->group(base_path('routes/web.php')); So you need to change it to resemble this:");
+                $this->comment("\n ->group(function () {");
+                $this->comment("\n require base_path('routes/web.php');");
+                $this->comment("\n require base_path('routes/queue.php');");
+                $this->comment("\n }");
+                $this->info("\n Finished setting up queue");
+            } else {
+                $this->info("\n You cancelled the grafite:queue");
+            }
+        }
+    }
+}
---
+++ src/Packages/Queue/app/Http/Controllers/Admin/LogsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace {{App\}}Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use {{App\}}Http\Controllers\Controller;
+use {{App\}}Services\LogService;
+
+class LogsController extends Controller
+{
+    public function __construct(LogService $logService)
+    {
+        $this->service = $logService;
+    }
+
+    /**
+     * Display a listing of the logs.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $logs = $this->service->get($request->all());
+        $levels = $this->service->levels;
+
+        return view('admin.logs.index')
+            ->with('logs', $logs)
+            ->with('levels', $levels);
+    }
+}
