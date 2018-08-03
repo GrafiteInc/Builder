4924ca689a58763c8d1266289a23f119890954f6
Matt Lantz <mattlantz@gmail.com>
2018-08-02 22:29:33 -0400

    5.7 fixes
    Along with some layout corrections

--- src/Packages/Bootstrap/resources/views/auth/passwords/email.blade.php
+++ src/Packages/Bootstrap/resources/views/auth/passwords/email.blade.php
@@ -11,15 +11,21 @@
             @include('partials.errors')
             @include('partials.status')
 
-            <div class="col-md-12 raw-margin-top-24">
-                <label>Email</label>
-                <input class="form-control" type="email" name="email" placeholder="Email" value="{{ old('email') }}">
+            <div class="row">
+                <div class="col-md-12 raw-margin-top-24">
+                    <label>Email</label>
+                    <input class="form-control" type="email" name="email" placeholder="Email" value="{{ old('email') }}">
+                </div>
             </div>
-            <div class="col-md-12 raw-margin-top-24">
-                <button class="btn btn-primary btn-block" type="submit" class="button">Send Password Reset Link</button>
+            <div class="row">
+                <div class="col-md-12 raw-margin-top-24">
+                    <button class="btn btn-primary btn-block" type="submit" class="button">Send Password Reset Link</button>
+                </div>
             </div>
-            <div class="col-md-12 raw-margin-top-24">
-                <a class="btn btn-link" href="/login">Wait I remember!</a>
+            <div class="row">
+                <div class="col-md-12 raw-margin-top-24">
+                    <a class="btn btn-link" href="/login">Wait I remember!</a>
+                </div>
             </div>
         </form>
 
--- src/Packages/Bootstrap/resources/views/auth/register.blade.php
+++ src/Packages/Bootstrap/resources/views/auth/register.blade.php
@@ -8,27 +8,36 @@
 
         <form method="POST" action="/register">
             {!! csrf_field() !!}
-
-            <div class="col-md-12 raw-margin-top-24">
-                <label>Name</label>
-                <input class="form-control" type="text" name="name" value="{{ old('name') }}" placeholder="Name">
+            <div class="row">
+                <div class="col-md-12 raw-margin-top-24">
+                    <label>Name</label>
+                    <input class="form-control" type="text" name="name" value="{{ old('name') }}" placeholder="Name">
+                </div>
             </div>
-            <div class="col-md-12 raw-margin-top-24">
-                <label>Email</label>
-                <input class="form-control" type="email" name="email" value="{{ old('email') }}" placeholder="Email">
+            <div class="row">
+                <div class="col-md-12 raw-margin-top-24">
+                    <label>Email</label>
+                    <input class="form-control" type="email" name="email" value="{{ old('email') }}" placeholder="Email">
+                </div>
             </div>
-            <div class="col-md-12 raw-margin-top-24">
-                <label>Password</label>
-                <input class="form-control" type="password" name="password" placeholder="Password">
+            <div class="row">
+                <div class="col-md-12 raw-margin-top-24">
+                    <label>Password</label>
+                    <input class="form-control" type="password" name="password" placeholder="Password">
+                </div>
             </div>
-            <div class="col-md-12 raw-margin-top-24">
-                <label>Confirm Password</label>
-                <input class="form-control" type="password" name="password_confirmation" placeholder="Password Confirmation">
+            <div class="row">
+                <div class="col-md-12 raw-margin-top-24">
+                    <label>Confirm Password</label>
+                    <input class="form-control" type="password" name="password_confirmation" placeholder="Password Confirmation">
+                </div>
             </div>
-            <div class="col-md-12 raw-margin-top-24">
-                <div class="btn-toolbar justify-content-between">
-                    <button class="btn btn-primary" type="submit">Register</button>
-                    <a class="btn btn-link" href="/login">Login</a>
+            <div class="row">
+                <div class="col-md-12 raw-margin-top-24">
+                    <div class="btn-toolbar justify-content-between">
+                        <button class="btn btn-primary" type="submit">Register</button>
+                        <a class="btn btn-link" href="/login">Login</a>
+                    </div>
                 </div>
             </div>
         </form>
